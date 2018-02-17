d580deed5c2dc766c243622d5fa912316659f0e2
Admin <Admin@mac2s-imac.fritz.box>
2018-02-17 11:49:12 +0100

    Initial Commit

---
+++ Taschenrechner-1-Tests/Taschenrechner_1_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Taschenrechner_1_Tests.swift
+//  Taschenrechner-1-Tests
+//
+//  Created by Admin on 17.02.18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import XCTest
+@testable import Taschenrechner_1_
+
+class Taschenrechner_1_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
